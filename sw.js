e9596366acaf36da8e737e1003b3ca0f0b0036a9
bullyto <apero.nuit.du.66@gmail.com>
2025-12-18 04:11:03 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>self.clients.claim());
+self.addEventListener("install", (e) => self.skipWaiting());
+self.addEventListener("activate", (e) => self.clients.claim());
